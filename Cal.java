87630f3fd04cd8605337a01ec99b31d0959979ef
RISHIGOLI <68457853+RISHIGOLI@users.noreply.github.com>
2022-07-01 14:53:00 +0530

    Update Cal.java

--- Cal.java
+++ Cal.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 class Cal{
   public static void main(String[] args) {
-
+    
+    System.out.println("Hello");
     char operator;
     Double number1, number2, result;
 
